4b1ee76a521ebb8e6ed4667aa93de6022c85d440
Tausif Ahmed <ahmedtausif5@gmail.com>
2021-07-13 19:36:52 +0600

    reduced dependencies

--- app.py
+++ app.py
@@ -1,44 +1,44 @@
-from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
-from werkzeug.datastructures import  FileStorage
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.layers import Dense, Activation
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.metrics import categorical_crossentropy
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Model
-from tensorflow.keras.applications import imagenet_utils
-from sklearn.metrics import confusion_matrix
-import itertools
-import os
-import shutil
-import random
-import matplotlib.pyplot as plt
-from tensorflow.keras.models import load_model
-import cv2
-from PIL import Image
-
-#################################################################
-
 # from flask import Flask, render_template, request
 # from werkzeug.utils import secure_filename
 # from werkzeug.datastructures import  FileStorage
 # import numpy as np
 # import tensorflow as tf
 # from tensorflow import keras
+# from tensorflow.keras.layers import Dense, Activation
+# from tensorflow.keras.optimizers import Adam
+# from tensorflow.keras.metrics import categorical_crossentropy
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
 # from tensorflow.keras.preprocessing import image
-# # from tensorflow.keras.models import Model
-# # import itertools
+# from tensorflow.keras.models import Model
+# from tensorflow.keras.applications import imagenet_utils
+# from sklearn.metrics import confusion_matrix
+# import itertools
 # import os
-# # import shutil
-# # import random
-# # import matplotlib.pyplot as plt
+# import shutil
+# import random
+# import matplotlib.pyplot as plt
 # from tensorflow.keras.models import load_model
 # import cv2
 # from PIL import Image
+
+#################################################################
+
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+# from tensorflow.keras.models import Model
+# import itertools
+import os
+# import shutil
+# import random
+# import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+import cv2
+from PIL import Image
 
 #################################################################
 
