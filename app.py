09fa69946d402e68eabfa4867f45f58312ac0b2f
Tausif Ahmed <77725879+ahmedtausif5@users.noreply.github.com>
2021-07-27 14:31:46 +0600

    changed threshold from 59 to 29

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ def classifiedPokemon():
                 list(label_dict.values()).index(out)]
 
             prob_1 = predictions[0][out]
-            if prob_1 >= 0.59:
+            if prob_1 >= 0.29:
                 # getting pokemon image directory
                 list_dir_string = f'static/{pokemon}'
                 pokemon_image_directory = listdir(list_dir_string)
